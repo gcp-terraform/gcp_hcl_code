e557da11de6cc8759adb2933f152d632279be4b5
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-11-24 11:04:55 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,73 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "newfile1101"
-  machine_type = "f1-micro"
-   zone        = "us-west4-b"
-  
-  tags         = ["externalssh","webserver"]
-  
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-7"
-    }
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./path/to/zip/file/which/contains/code"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs14"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+Example Usage - Single User
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = ""
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs14"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "helloGET"
+  labels = {
+    my-label = "my-label-value"
   }
-  network_interface {
-    network = "default"
-    
+
+  environment_variables = {
+    MY_ENV_VAR = "my-env-var-value"
   }
+}
+
+# IAM entry for a single user to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "user:myFunctionInvoker@example.com"
 }
